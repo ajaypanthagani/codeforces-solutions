76c2a0d763a4919847dcbb9783509dd422ab5984
AJAY PANTHAGANI <ajaypanthagani321@gmail.com>
2019-12-30 21:04:02 +0530

    modified 721A

--- 721A.cpp
+++ 721A.cpp
@@ -5,29 +5,36 @@ using namespace std;
 
 int main(){
 
-	int n, size=0, flag = 0;
+	int n, flag = 0;
 	char c;
 
 	std::vector<int> v;
 
 	cin>>n;
-	while(n--){
+	for(int i=1; i<=n; i++){
 
 		cin>>c;
-		if(c=='B' && flag==0){
-			size=1;
-			flag=1;
+		if(c=='B'){
+			if(flag==0){
+				flag=1;
+			}
+			else if(flag>0){
+				flag++;
+			}
+			if(i==n){
+				v.push_back(flag);
+			}
 		}
-		else if(c=='B' && flag==1){
-			size++;
-		}
-		if(c=='W' && flag==1){
-			v.push_back(size);
-			size=0;
+		else if((c=='W' && flag>0)){
+			v.push_back(flag);
 			flag=0;
 		}
 
 	}
+
+	// if(c=='B'){
+	// 	v.push_back(flag);
+	// }
 
 	cout<<v.size()<<endl;
 
